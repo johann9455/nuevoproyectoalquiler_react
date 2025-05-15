468281ac40da83e4251a1464a879b8be33eb013f
Esteban <estebanospina538@gmail.com>
2025-05-15 16:16:19 -0500

    fake Api

---
+++ alquiler-carros/src/pages/fakeApi.js
@@ -0,0 +1,19 @@
+const BASE_DE_DATOS = Array.from({ length: 60 }, (_, i) => ({
+  id: i + 1,
+  marca: ["Toyota", "Nissan", "BMW", "Kia"][i % 4],
+  modelo: ["Corolla", "Sentra", "X5", "Sportage"][i % 4],
+  precioDia: Math.floor(Math.random() * 150000) + 50000, // Precio entre 50k y 200k
+  tipo: ["Sedán", "SUV", "Deportivo", "Hatchback"][i % 4],
+  año: 2020 + (i % 5),
+  transmision: ["Automático", "Manual"][i % 2],
+  descripcion: `Vehículo en excelente estado, ${["aire acondicionado", "tecnología Bluetooth", "asientos de cuero"][i % 3]}.`,
+}));
+
+export function obtenerProductos(pagina, tamañoPagina = 15) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const inicio = (pagina - 1) * tamañoPagina;
+      resolve(BASE_DE_DATOS.slice(inicio, inicio + tamañoPagina));
+    }, 800);
+  });
+}
