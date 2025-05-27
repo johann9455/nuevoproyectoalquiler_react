fa85c8192b4900220c7df9b17254537d5c592e1d
Esteban <estebanospina538@gmail.com>
2025-05-27 16:00:59 -0500

    Retoque de imagenes y confirmacion 3

--- alquiler-carros/src/pages/fakeApi.js
+++ alquiler-carros/src/pages/fakeApi.js
@@ -7,7 +7,7 @@ const BASE_DE_DATOS = Array.from({ length: 60 }, (_, i) => ({
   año: 2020 + (i % 5),
   transmision: ["Automático", "Manual"][i % 2],
   descripcion: `Vehículo en excelente estado, ${["aire acondicionado", "tecnología Bluetooth", "asientos de cuero", "GPS integrado", "sensores de parqueo", "cámara trasera"][i % 6]}.`,
-  imagen: ["/img/1.png","/img/2.png","/img/3.png","/img/4.png","/img/ford.jpg","/img/camaro.jpg"][i % 6],
+  imagen: ["/img/corolla.jpg","/img/nissan.jpg","/img/bmw.jpg","/img/kia.jpeg","/img/ford.jpg","/img/camaro.jpg"][i % 6],
 }));
 
 export function obtenerProductos(pagina, tamañoPagina = 15) {
